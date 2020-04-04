404e67658b63b433ea62cfc45095cd47c9a0b6a0
Cherina97 <Cherina97@gmail.com>
2020-04-04 17:22:40 +0300

    add hibernate

---
+++ src/main/java/org/example/CartEntity.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cart", schema = "application", catalog = "")
+public class CartEntity {
+    private Integer id;
+    private String total;
+    private String name;
+
+    @Id
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "total")
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartEntity that = (CartEntity) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(total, that.total) &&
+                Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, total, name);
+    }
+}
